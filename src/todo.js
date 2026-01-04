e26ec2e020e436973e85c1358dbbc8fa71185c82
eldonaukh <eldonakh@gmail.com>
2026-01-04 23:39:37 +0800

    Export Todo

--- src/todo.js
+++ src/todo.js
@@ -1,3 +1,5 @@
+import { el } from "./utils.js";
+
 class Todo {
   constructor(id, project, title, desc, dueDate, priority) {
     this.id = id;
@@ -5,6 +7,16 @@ class Todo {
     this.desc = desc;
     this.dueDate = dueDate;
     this.priority = priority;
+    this.completed = false;
     this.project = project;
   }
+
+  renderTodoSimple() {
+    const infoDIv = el("div", {}, [
+      el("h4", {}, `${this.project.tasks.indexOf(this) + 1}. ${this.title}`),
+    ]);
+    return infoDIv;
+  }
 }
+
+export { Todo };
